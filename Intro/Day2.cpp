8fbbc53e40669a20ca31ce298baf8258ffbbf9f5
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-08-28 09:22:42 +0530

    Create Day2.cpp

---
+++ Intro/Day2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    bool d;
+    cout<<"size of int"<<sizeof(a)<<endl;
+    cout<<"size of float"<<sizeof(b)<<endl;
+    cout<<"size of char"<<sizeof(c)<<endl;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+    return 0;
+}
