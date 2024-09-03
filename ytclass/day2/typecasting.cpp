fa4eb6ea103d1a2d5d6dfb6deaa298f2d2de2e81
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-03 23:53:49 +0530

    Create typecasting.cpp

---
+++ ytclass/day2/typecasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespcae std;
+int main() {
+char grade = 'a';
+    
+    int value = grade;
+    cout<<value;
+
+  double price = 100.99;
+    int value = (int)price;
+    
+    cout<<value;
+    return 0;
+}
