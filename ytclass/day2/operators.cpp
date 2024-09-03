8281cfd3f9cdb6c443f00488ace43ca1976af89a
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-04 00:14:30 +0530

    Create operators.cpp

---
+++ ytclass/day2/operators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //arthmetic operators
+    cout<<"sum="<<(2+3)<<endl;
+    cout<<"diff="<<(2-3)<<endl;
+    cout<<"prod="<<(2*3)<<endl;
+    cout<<"quo="<<(2/3)<<endl;
+    cout<<"rem="<<(2%3)<<endl;
+    
+    //relational operators
+    cout<<(2>3)<<endl;
+    cout<<(2>=3)<<endl;
+    cout<<(2<3)<<endl;
+    cout<<(2<=3)<<endl;
+    cout<<(2==3)<<endl;
+    cout<<(2!=3)<<endl;
+    
+    
+    //logical operators
+    // or ||
+    // and &&
+    cout<<!(3<2)<<endl;
+    cout<<(3<2)<<endl;
+    
+    return 0;
+}
