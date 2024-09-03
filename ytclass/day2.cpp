9dddc2ce954a2b29463aff0016141e796cdc435e
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-03 23:30:51 +0530

    Create day2.cpp

---
+++ ytclass/day2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello world"<<"\n";
+    cout<<"Edwina\nDsouza"<<endl;
+    
+    int age = 25;
+    cout<<"25"<<"\n";
+    cout<<age;
+    return 0;
+}
