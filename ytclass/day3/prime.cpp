54c3c86a5ad0f851b65121a5a30c016207adad44
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-05 11:21:39 +0530

    Update prime.cpp

--- ytclass/day3/prime.cpp
+++ ytclass/day3/prime.cpp
@@ -6,7 +6,7 @@ int main() {
     cout<<"enter a no:";
     cin>>n;
     bool isprime = true;
-    for(int i=2;i<n;i++){
+    for(int i=2;i<n;i++){ //for(int i=2;i*i<=n;i++)- optimal
         if(n%i==0){
             isprime = false;
             break;
