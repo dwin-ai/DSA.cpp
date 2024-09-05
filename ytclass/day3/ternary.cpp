63b896374ec38b5dcde67a65090afdf0b1b1237f
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-05 10:28:20 +0530

    Create ternary.cpp

---
+++ ytclass/day3/ternary.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter a no:";
+    cin>>n;
+    
+    cout<<(n>=0?"positive":"negative"); //ternary statement
+    return 0;
+}
