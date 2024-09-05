f42903f74f16ffaaa96936c2de9d4721c0c2acb1
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-05 10:47:58 +0530

    Update sumloop.cpp

--- ytclass/day3/sumloop.cpp
+++ ytclass/day3/sumloop.cpp
@@ -9,7 +9,15 @@ int main()
     int sum = 0;
     for(int i =1;i<=n;i++){
         sum = sum +i;
+    } // for
+    //while
+    int sum = 0;
+    int i = 1;
+    while(i<=n){
+        sum+=i;
+        i++;
     }
     cout<<sum;
     return 0;
 }
+   
