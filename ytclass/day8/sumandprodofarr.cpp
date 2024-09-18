5726c2e995f63ba11bc88daa82640c580b88e724
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-09-18 11:00:40 +0530

    Update sumandprodofarr.cpp

--- ytclass/day8/sumandprodofarr.cpp
+++ ytclass/day8/sumandprodofarr.cpp
@@ -5,11 +5,14 @@ int main() {
     int arr[]={1,4,6,7,3};
     int sz = sizeof(arr)/sizeof(int);
     int sum=0;
+    int prod=1;
     
     for (int i=0;i<sz;i++){
         sum +=arr[i];
+        prod *=arr[i];
         
     }
-    cout<<sum;
+    cout<<sum<<endl;
+    cout<<prod;
     return 0;
 }
